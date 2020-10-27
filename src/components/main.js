f0ccc0d8e69838aba47d2c2f072d0b5de1797c55
burak-karaca <pwr284812@gmail.com>
2020-10-27 16:18:16 +0100

    commit

---
+++ src/components/main.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function main() {
+    return (
+        <div>
+        <div className="page">
+        <div className="block">
+        <div className="header">
+        <h2>Login to view our user pool</h2>
+        </div>
+        <div className="form">
+        <form>
+        <input type="email" placeholder="EMAIL"></input>
+        <input type="password" placeholder="PASSWORD"></input>
+        <br></br>
+        <button>Login</button>
+        </form></div>
+        
+        </div>
+    </div>
+        </div>
+    )
+}
+
+export default main
